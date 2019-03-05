91297fd1db1b6096f84af04ca312140f8a907c91
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-03-05 10:36:11 -0500

    Operadores Kotlin

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
     println("Hola Mundo")
+    println(1+1)
+    println(3-1)
+    println(2*2)
+    println(4/2)
+    println(7%2)
+
+    val a = 4
+    val b = 2
+
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+
 }
