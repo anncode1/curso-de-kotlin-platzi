bf302e15c033a3b4e740b3844e76c3e593c85398
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-03-06 11:21:44 -0500

    Array

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -111,6 +111,25 @@ fun main(args: Array<String>) {
     val listWithoutNulls: List<Int?> = listWithNulls.filterNotNull()
     println(listWithoutNulls)
 
+    //Array
+    val countries
+            = arrayOf("India", "Mexico", "Colombia", "Argentina", "Chile", "Peru")
+
+    val days = arrayOf<String>("Lunes", "Martes", "Miercoles")
+
+
+
+    //Promedio de los numeros
+    val numbers = intArrayOf(6,6,23,9,2,3,2)
+    var sum = 0
+    for (num in numbers){
+        sum += num
+    }
+
+    val average = sum / numbers.size
+    println("Promedio: $average")
+
+
 
 }
 
