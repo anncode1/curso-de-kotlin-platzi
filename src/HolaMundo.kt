64e65e982e6598ba4099b3c3ea294a0be4c8bc4e
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-03-06 09:41:33 -0500

    while y  do while

--- src/HolaMundo.kt
+++ src/HolaMundo.kt
@@ -71,6 +71,20 @@ fun main(args: Array<String>) {
     }
 
 
+    var i = 1
+    //while contadores
+
+    while (i < 1 ){//false true
+        println("mensaje: $i")
+        i++
+    }
+
+    i = 1
+    do {
+        println("mensaje dowhile: $i")
+        i++
+    }while (i < 1 )
+
 
 }
 
